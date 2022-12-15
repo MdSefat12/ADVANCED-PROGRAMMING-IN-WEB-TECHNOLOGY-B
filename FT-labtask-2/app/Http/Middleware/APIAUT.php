78ce0f2233690f6ab49caf87738beb405ea9b523
MD SEFAT <tanjimsefat12@gmail.com>
2022-12-15 22:24:59 +0600

    plan view,product view,and plan create using react

---
+++ FT-labtask-2/app/Http/Middleware/APIAUT.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\token;
+use Closure;
+use Illuminate\Http\Request;
+
+
+class APIAUT
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $token = $request->header("Authorization");
+        $check_token = token::where("token",$token)->where("expired_at",NULL)->first();
+        if($check_token)
+         {
+            return $next($request);
+        }
+       
+        else return response("invalid token",401);
+    }
+}
